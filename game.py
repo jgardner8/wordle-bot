be5e40e90109cb99d16bcad574f5e063280f0ace
James Gardner <james.gardner@rea-group.com>
2023-06-14 12:39:01 +1000

    Add animated guess grid to the end

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
+import time
 import ai_player
 from game_data import Guess, Match
+from list import find
 
 
 def _is_game_won(guesses):
@@ -16,7 +18,7 @@ def _ask_user_for_matches(guess_str):
     matches = []
     i = 0
     while i < 5:
-        print("Letter", i + 1, "matched? (g=green, y=yellow, n=grey)")
+        print("Letter", i + 1, "matched? (g=green, y=yellow, n=grey) ", end="")
         feedback = input()
         if feedback not in ["g", "y", "n"]:
             continue
@@ -27,11 +29,37 @@ def _ask_user_for_matches(guess_str):
     return matches
 
 
+def _print_guess_grid(guesses):
+    def print_char(c):
+        print(c, end="", flush="True")
+        time.sleep(0.1)
+
+    for guess_idx in range(0, len(guesses)):
+        print_char(guess_idx + 1)
+        print_char(":")
+        print_char(" ")
+
+        for char_idx in range(0, 5):
+            match = find(lambda m: m.index == char_idx, guesses[guess_idx].matches)
+            if match:
+                if match.index_matched:
+                    print_char("g")
+                else:
+                    print_char("y")
+            else:
+                print_char(" ")
+
+        print("")
+
+    print("")
+
+
 def game_loop(words):
     guesses = []
     for turn_number in range(1, 7):
         if _is_game_won(guesses):
             print("Winner winner, chicken dinner!")
+            _print_guess_grid(guesses)
             break
 
         print("Turn", turn_number)
@@ -40,6 +68,8 @@ def game_loop(words):
         guess_str = ai_player.next_guess(words, guesses)
         if guess_str == None:
             print("Oops, the word is not in words.txt :(")
+            _print_guess_grid(guesses)
+            break
         else:
             matches = _ask_user_for_matches(guess_str)
             guesses.append(Guess(guess_str, matches))
